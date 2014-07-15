4d7cb4a869391b613a67a698689952e7d8570c38
Ravi <upman16@gmail.com>
2014-07-15 19:21:37 +0530

    Got a basic user pairing and messaging interface working

---
+++ app.rb
@@ -0,0 +1,65 @@
+require 'thin'
+require 'em-websocket'
+require 'sinatra/base'
+require 'digest'
+require 'base64'
+require 'json'
+
+def uid(ws)
+	#for complex looking uids :D
+	#sha1 = Digest::SHA256.new
+	#Base64.encode64(sha1.digest ws.object_id.to_s)
+
+	#simpler integer uids
+	return ws.object_id
+end
+
+EM.run do
+	class App < Sinatra::Base
+		get '/' do
+			erb :index
+		end
+	end
+
+	@opponents = Hash.new(nil)
+	@userwaiting = nil
+
+	EM::WebSocket.start(:host => '0.0.0.0', :port => '3001') do |ws|
+		ws.onopen do |handshake|
+			
+			if @userwaiting == nil
+				@userwaiting = ws
+				@userwaiting.send "userid: #{uid(@userwaiting)}"
+			else	
+			#There is a user waiting to be paired up
+			@opponents[uid(ws)] = { 'oppid' => uid(@userwaiting), 'socket' => @userwaiting}
+			@opponents[uid(@userwaiting)] = {'oppid'=> uid(ws), 'socket' => ws} 
+			ws.send "userid: #{uid(ws)}"
+			ws.send "opponentid: #{uid(@userwaiting)}"
+			@userwaiting.send "opponentid: #{uid(ws)}"
+			@userwaiting = nil
+			end
+		end
+
+		ws.onclose do 
+			opp = @opponents[uid(ws)]
+			opp['socket'].send 'message: Opponent disconnected. Close the socket to bugger off.'
+
+			puts JSON.pretty_generate @opponents
+			oppid = opp['oppid']
+			@opponents.delete oppid
+			@opponents.delete uid(ws)		
+			puts JSON.pretty_generate @opponents
+		end
+
+		ws.onmessage do |msg|
+			if @opponents[uid(ws)] == nil
+				ws.send 'You have no opponent yet, please wait.'
+			else
+				@opponents[uid(ws)]['socket'].send msg
+			end
+		end
+	end
+
+	App.run! :port => 3000
+end
